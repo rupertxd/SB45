a13dfd69b8a518945cfb7188e719f2f64d846838
RupertXD <rupert.chen@dexatek.com>
2015-05-18 07:46:54 +0800

    got over the hell of XML, first working command-line prototype

--- sb.php
+++ sb.php
@@ -18,15 +18,28 @@ else {
 foreach ($kwds as $kwd) {
 	$url = str_replace('_KEYWORD_', urlencode($kwd), $URL_GSADV);
 	$advstr = file_get_contents($url);
-	echo "$advstr\n";
+	// echo "$advstr\n";
 	//$advxm = new SimpleXMLElement($advstr);
 	//print_r($advxm);
-	$xmi = new SimpleXMLIterator($advstr);
-	for ($xmi->rewind(); $xmi->valid(); $xmi->next()) {
-		foreach ($xmi->getChildren() as $key=>$val) {
+	$xml = new SimpleXMLIterator($advstr);
+	// <toplevel>
+	// <CompleteSuggestion>
+	// <suggestion data="14th amendment"/>
+	// </CompleteSuggestion>
+	$xmi = $xml->xpath('/toplevel/CompleteSuggestion/suggestion');
+	// print_r($xmi);
+	// echo "###\n";
+	foreach ($xmi as $node) {
+		// print_r($node);
+		// echo "---\n";
+		$natts = $node->attributes();
+		echo "{$natts['data']}\n";
+		/*
+		foreach ($xmn->attributes() as $key=>$val) {
 			echo "$key: $val\n";
-			echo "{$val->attributes('data')}\n";
 		}
+		*/
 	}
+		
 
 }
